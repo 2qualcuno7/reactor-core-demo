b5715072147a9afa52f198a8108463ac95c5c2e3
Juan Carlos Suarez <suarezmjuan@gmail.com>
2021-07-18 18:08:21 -0500

    Creates Flux

--- src/main/java/com/jcsm/project1/Project1Application.java
+++ src/main/java/com/jcsm/project1/Project1Application.java
@@ -1,13 +1,22 @@
 package com.jcsm.project1;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class Project1Application {
+public class Project1Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project1Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> names = Flux.just("Juan", "Maria", "Pedro", "Paula")
+				.doOnNext(System.out::println);
+
+		names.subscribe();
+	}
 }
