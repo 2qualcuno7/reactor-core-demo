c4b8ef4cd9a092cfc3713fc080c9a5f6dd8a5f11
Juan Carlos Suarez <suarezmjuan@gmail.com>
2021-07-18 18:19:56 -0500

    Error managing with subscribe method

--- src/main/java/com/jcsm/project1/Project1Application.java
+++ src/main/java/com/jcsm/project1/Project1Application.java
@@ -1,12 +1,18 @@
 package com.jcsm.project1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
 
+
+
 @SpringBootApplication
 public class Project1Application implements CommandLineRunner {
+
+	private static final Logger log = LoggerFactory.getLogger(Project1Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project1Application.class, args);
@@ -14,9 +20,16 @@ public class Project1Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> names = Flux.just("Juan", "Maria", "Pedro", "Paula")
-				.doOnNext(System.out::println);
+		Flux<String> names = Flux.just("Juan", "Maria", "Pedro", "", "Paula")
+				.doOnNext(element -> {
+					if(element.isEmpty()){
+						throw new RuntimeException("EMPTY_ELEMENT");
+					}
+					else{
+						System.out.println(element);
+					}
+				});
 
-		names.subscribe();
+		names.subscribe(element -> log.info(element), error -> log.error(error.getMessage()));
 	}
 }
